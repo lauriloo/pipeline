4610fd73640798ab2d427cf1daee3adb69de6f71
Lauri <lauri.meriloo@gmail.com>
2017-04-03 00:57:10 +0300

    changes in Main to check if fits works

--- src/main/java/ee/meriloo/toru/Main.java
+++ src/main/java/ee/meriloo/toru/Main.java
@@ -1,7 +1,9 @@
 package ee.meriloo.toru;
 
+import ee.meriloo.toru.model.Junction;
 import ee.meriloo.toru.model.Pipeline;
 import ee.meriloo.toru.model.Rectangle;
+import ee.meriloo.toru.service.RectangleFitCheckService;
 import ee.meriloo.toru.service.RectangleSlidingService;
 import ee.meriloo.toru.service.input.InputParsingService;
 import ee.meriloo.toru.service.RectangleMovingService;
@@ -26,9 +28,11 @@ public class Main {
     @Autowired
     private InputParsingService inputParsingService;
     @Autowired
-    private RectangleMovingService rectangleMovingService;
+    private RectangleMovingService movingService;
     @Autowired
-    private RectangleSlidingService rectangleSlidingService;
+    private RectangleSlidingService slidingService;
+    @Autowired
+    private RectangleFitCheckService fitCheckService;
 
 
     public static void main(String[] args) throws Exception {
@@ -36,18 +40,26 @@ public class Main {
         Main main = context.getBean(Main.class);
 
 //        String input = "60, 120, 100, 75";
-        String input = "100, 35, 75, 50, 80, 100, 37";
-//        String input = "70, 50, 60, 60, 55, 90";
+//        String input = "100, 35, 75, 50, 80, 100, 37";
+        String input = "70, 50, 60, 60, 55, 90";
         List<BigDecimal> rectangleDimensions = main.inputParsingService.getRectangleDimension(input);
         List<BigDecimal> pipesDiameters = main.inputParsingService.getPipesDiameters(input);
         Rectangle rectangle = main.rectangleBuilder.buildRectangle(rectangleDimensions);
         Pipeline pipeline = main.pipelineBuilder.buildPipeLine(pipesDiameters);
-        main.rectangleMovingService.moveToJunctionCorner(rectangle, pipeline.getJunctions().get(0));
+        Junction junction = pipeline.getJunctions().get(2);
+        main.movingService.moveToJunctionCorner(rectangle, junction);
 
         boolean slidingHasNotEnded = true;
-        BigDecimal step = rectangle.getLength().divide(BigDecimal.valueOf(100000.0));
+        BigDecimal step = rectangle.getLength().divide(BigDecimal.valueOf(100.0));
         while (slidingHasNotEnded) {
-            slidingHasNotEnded = main.rectangleSlidingService.slideRectangleRight(rectangle, pipeline.getJunctions().get(0), step);
+            slidingHasNotEnded = main.slidingService.slideRectangleRight(rectangle, junction, step);
+            boolean fits = main.fitCheckService.fits(rectangle);
+            if (!fits) {
+                slidingHasNotEnded = false;
+                System.out.println("Does not fit");
+            } else if (!slidingHasNotEnded) {
+                System.out.println("Fits");
+            }
         }
 
         System.out.println();
